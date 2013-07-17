8a14e21d184c97a43bf9de16b81337c739d521be
Alex Karpov <alexakarpov@gmail.com>
2013-07-17 14:44:38 -0400

    done with naive boggler, now to make proper use of the trie

--- boggler.py
+++ boggler.py
@@ -1,7 +1,13 @@
-test_board = [('a', 'b', 'c', 'd'),
-              ('a', 'b', 'c', 'd'),
-              ('a', 'b', 'c', 'd'),
-              ('a', 'b', 'c', 'd')]
+BOARD_SIZE = 3
+MIN_WORD_LENGTH = 3
+
+test_board = [('a', 'b', 'c'),
+              ('d', 'e', 'f'),
+              ('g', 'h', 'i')]
+
+test_board2 = [('b', 'a', 'r'),
+              ('e', 'c', 'n'),
+              ('l', 'a', 'o')]
 
 
 def is_acceptable(word):
@@ -28,6 +34,70 @@ dictionary_list = filter_acceptable([line for line in open('/usr/share/dict/word
 print("built a dictionary of %d English words" % (len(dictionary_list)))
 # ..and turn it into a trie.
 
+def get_neighbours(v):
+    row = v[0]
+    column = v[1]
+    return [(r, c) for r in [row - 1, row, row + 1] for c in [column - 1, column, column + 1]
+            if (not (r == row and c == column)) and (1 <= r <= BOARD_SIZE and 1 <= c <= BOARD_SIZE)]
+
+assert get_neighbours((2, 2)) == [(1, 1), (1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2), (3, 3)]
+assert get_neighbours((1, 1)) == [(1, 2), (2, 1), (2, 2)]
+
+
+def dfs(graph, vertex):
+    """
+    graph - a square list of tuples,
+    vertex - a Node representing the starting cell
+    """
+    results = []
+
+    def visit(node, visited, word_so_far):
+        visited.append(node)
+        word_so_far += graph[node[0] - 1][node[1] - 1]
+        if len(word_so_far) >= MIN_WORD_LENGTH:
+            results.append(word_so_far)
+        neighbours = get_neighbours(node)
+        for next_coordinates in neighbours:
+            next_node = (next_coordinates[0], next_coordinates[1])
+            if not next_node in visited:
+                visit(next_node, list(visited), word_so_far)
+
+    visit(vertex, [vertex], "")
+    return results
+
+
+class BoggleSolver:
+    def __init__(self, list_of_lists):
+        assert len(list_of_lists) == BOARD_SIZE
+        assert all(len(row) == BOARD_SIZE for row in list_of_lists)
+        self.set_game_board(list_of_lists)
+
+    def set_game_board(self, list_of_lists):
+        self.board = list_of_lists
+
+    def print_board(self):
+        for row in self.board:
+            print(row)
+
+    def play(self):
+        return []
+
+# Now let's play some Boggle!
+
+result = []
+for start in [(r, c) for r in range(1, BOARD_SIZE + 1) for c in range(1, BOARD_SIZE + 1)]:
+    words_at_start = dfs(test_board2, start)
+    print("found %d paths starting at %s" %(len(words_at_start), start,))
+    result = result + words_at_start
+
+for line in result:
+    if line in dictionary_list:
+        print("found a word! : %s" % line)
+
+
+# OK that works, but it is slow as hell, because we keep looking for all possible paths, even when it's obviously not
+# leading to any real word! We need to add pruning of hopeless paths, and we can do it if we convert our dictionary into
+# a TRIE.
 
 def build_trie(words):
     """
@@ -52,54 +122,6 @@ def build_trie(words):
         # start adding letters to the dict on the corresponding level, beginning with root
         for letter in word:
             current_dict = current_dict.setdefault(letter, {})
-        # a word is now represented with keys in nested dicts -- terminate it with None
+            # a word is now represented with keys in nested dicts -- terminate it with None
         current_dict = current_dict.setdefault(None, None)
     return root_dict
-
-
-class Node:
-    def __init__(self, row, column, letter):
-        assert 1 <= row <= 4 and 1 <= column <= 4
-        self.row = row
-        self.column = column
-        # self.up = (row - 1 if row > 1 else None)
-        # self.down = (row + 1 if row < 3 else None)
-        # self.left = (column - 1 if column > 1 else None)
-        # self.right = (column + 1 if column < 3 else None)
-        self.letter = letter
-
-
-def get_neighbours(row, column):
-    return [(r, c) for r in [row - 1, row, row + 1] for c in [column - 1, column, column + 1]
-            if (not (r == row and c == column)) and not (r == 0 or c == 0)]
-
-assert get_neighbours(2, 2) == [(1, 1), (1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2), (3, 3)]
-assert get_neighbours(1, 1) == [(1, 2), (2, 1), (2, 2)]
-
-
-def dfs(graph, vertex):
-    """
-    graph - a square list of tuples,
-    vertex - a Node representing the starting cell
-    """
-
-
-class BoggleSolver:
-    def __init__(self, list_of_lists):
-        assert len(list_of_lists) == 4
-        assert all(len(row) == 4 for row in list_of_lists)
-        self.set_game_board(list_of_lists)
-
-    def set_game_board(self, list_of_lists):
-        self.board = list_of_lists
-
-    def print_board(self):
-        for row in self.board:
-            print(row)
-
-    def play(self):
-        return []
-
-
-game = BoggleSolver(test_board)
-game.print_board()
